8ac602ec56b8266f9251de5b2a926862b319bfe0
Bhavani0106 <yarrambhavani6@gmail.com>
2023-11-29 20:01:59 +0530

    first commit

---
+++ array.java
@@ -0,0 +1,12 @@
+ public class Test{
+    public static void main(String[]args){
+        int[] size = new int[5];
+        String[] names =new String[5];
+        System.out.println(size[0]);
+        System.out.println(size[5]);
+
+        System.out.println(names[0]);
+        System.out.println(names[5]);
+
+    }
+}
---
+++ data.java
@@ -0,0 +1,4 @@
+//primitive data types
+    int acc_id =101;
+    String acc_name ="Bhavani";
+    double acc_Amount =500;
